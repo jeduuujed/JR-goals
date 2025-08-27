04b6b6f7b35890f1d8ad3cecf80bbe97b3b4fd60
riedamae <ygayrieda08@gmail.com>
2025-08-27 09:46:54 +0800

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9SSoacNEUX6A1pfXFQxEzWZRJwkTRoZs",
+  authDomain: "jr-goals.firebaseapp.com",
+  projectId: "jr-goals",
+  storageBucket: "jr-goals.firebasestorage.app",
+  messagingSenderId: "837077227533",
+  appId: "1:837077227533:web:fd3a4d05e9c330c4669d44"
+};
+
+
+const app = initializeApp(firebaseConfig);
